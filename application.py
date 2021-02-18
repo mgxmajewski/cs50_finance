298a04168374248e791637c8bf3b8c6a1b7ded36
mgxmajewski <mgxmajewski@gmail.com>
2021-02-18 02:18:00 +0000

    Fix big in sell - shares_available db statement

--- application.py
+++ application.py
@@ -301,7 +301,8 @@ def sell():
     user_stocks = []
 
     for stocks in user_shares:
-        user_stocks.append(stocks['symbol'])
+        if stocks['sum(transactions.shares)'] > 0:
+            user_stocks.append(stocks['symbol'])
     
     if request.method == "POST":
         
@@ -321,10 +322,19 @@ def sell():
             shares_to_sell = int(shares_to_sell)
         
         # Calculate stock
-        shares_available = db.execute("SELECT sum(transactions.shares) FROM transactions JOIN stocks ON stocks.id = transactions.stock_id WHERE user_id=:user_id GROUP BY stock_id", user_id=user_id)[0]['sum(transactions.shares)']
+        shares_available = db.execute("SELECT sum(transactions.shares) FROM transactions JOIN stocks ON stocks.id = transactions.stock_id WHERE user_id=:user_id AND symbol=:symbol ", user_id=user_id, symbol= stock_to_sell)[0]['sum(transactions.shares)']
         
-        if shares_to_sell <= shares_available:
-             return apology("Exceeded your amount of shares to sell", 400)
+        
+        print('shares_to_sell', shares_to_sell)
+        print('shares_available', shares_available )
+        print('shares_to_sell type ', type(shares_to_sell))
+        print('shares_available type ', type(shares_available))
+        
+        if shares_to_sell == shares_available:
+            print("equal")
+            
+        elif shares_to_sell > shares_available:
+            return apology("Exceeded your amount of shares to sell", 400)
             
         # Calculate values for db upadate
         
@@ -342,10 +352,11 @@ def sell():
         
         print(usd(balance_update))
         
-        # Db update
-        stock_db_id = db.execute("SELECT id FROM stocks WHERE symbol = ?", iex_symbol)[0]['id']
-        db.execute("INSERT INTO transactions (stock_id, user_id, shares, price) VALUES (?, ?, ?, ?)", stock_db_id, user_id, shares_update, iex_price)
-        db.execute("UPDATE users SET cash= ? WHERE id= ? ", balance_update, user_id)
+        if shares_to_sell <= shares_available:
+            # Db update
+            stock_db_id = db.execute("SELECT id FROM stocks WHERE symbol = ?", iex_symbol)[0]['id']
+            db.execute("INSERT INTO transactions (stock_id, user_id, shares, price) VALUES (?, ?, ?, ?)", stock_db_id, user_id, shares_update, iex_price)
+            db.execute("UPDATE users SET cash= ? WHERE id= ? ", balance_update, user_id)
         
         return redirect("/")
         
