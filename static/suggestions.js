0ac0482b5e5864d899c06a364f85171427a95a6e
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-11-12 11:14:43 +0100

    Create logic for "keyup" addEventListener and log parsed response.

--- static/suggestions.js
+++ static/suggestions.js
@@ -1,5 +1,5 @@
 const suggestionsEndpoint = (phrase) => `http://localhost:5000/suggestions?phrase=${phrase.toUpperCase()}`
-
+const inputDiv = document.getElementById("myInput")
 // Handle fetch request
 async function fetchStocks(url) {
     try {
@@ -10,6 +10,18 @@ async function fetchStocks(url) {
     }
 }
 
-fetchStocks(suggestionsEndpoint('app')).then(results => FetchedStocks(results))
+const parseSuggestions = array => array.map(stock => `${stock.symbol} - ${stock.company_name}`)
+
+const logFetchedStocks = results => console.log(results)
+
+inputDiv.addEventListener("keyup", (e) => {
+    e.preventDefault()
+    if(e.target.value !== ''){
+        fetchStocks(suggestionsEndpoint(e.target.value))
+            .then(results => parseSuggestions(results))
+            .then(parsedSuggestions => logFetchedStocks(parsedSuggestions))
+    }
+});
+
 
 const FetchedStocks = results => console.log(results)
