a341fb595389abb399c9937c46e9b5ead3c51364
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-03-27 11:11:29 +0200

    Fix lookup (workaround cs50 lack of UNION support).

--- routes/suggestions.py
+++ routes/suggestions.py
@@ -8,13 +8,22 @@ from . import routes
 @login_required
 def suggestions():
     phrase = request.args.get('phrase')
-    phrase_regex = str(phrase+'%')
+    phrase_regex = str(phrase)
 
-    result = db.execute("SELECT symbol, company_name "
-                        "FROM stocks WHERE symbol "
-                        "LIKE :lookup_regex "
-                        "ORDER BY symbol "
-                        "ASC LIMIT 5",
-                        lookup_regex=phrase_regex)
+    symbol_result = db.execute("SELECT symbol, company_name "
+                               "FROM stocks WHERE symbol "
+                               "LIKE ? "
+                               "ORDER BY symbol "
+                               "ASC LIMIT 5",
+                               '%' + phrase_regex + '%')
 
-    return jsonify(result)
+    company_name_result = db.execute("SELECT symbol, company_name "
+                                     "FROM stocks WHERE company_name "
+                                     "LIKE ? "
+                                     "ORDER BY company_name "
+                                     "ASC LIMIT 5",
+                                     '%' + phrase_regex + '%')
+
+    merged_result = company_name_result + symbol_result
+
+    return jsonify(merged_result)
