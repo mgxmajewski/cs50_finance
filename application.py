d259341b395ae98e84c5d6b4aee94d1ecc0af1ae
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-03-27 21:47:49 +0200

    Add Cors to app.

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -11,6 +12,7 @@ from helpers import apology, usd
 
 # Configure application
 app = Flask(__name__)
+CORS(app)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
